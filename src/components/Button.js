83ee170dfcf95ad7dc647b624b8de1e7b159ed95
dsudasinghe <dananjayasudasinghe@gmail.com>
2021-08-11 18:50:06 +0530

    updated

---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+const Button = ({text,color}) => {
+    return (
+        <button className="btn" style={{backgroundColor :color}}>{text}</button>
+    )
+}
+
+Button.defaultProps = {
+    color : 'steelblue'
+}
+
+Button.propTypes = {
+    text : PropTypes.string,
+    color : PropTypes.string,
+}
+
+export default Button
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import Button from './Button';
+import PropTypes from 'prop-types' ;
 
-const Header = () => {
+const Header = ({title}) => {
     return (
-        <header>
-            <h1>Task Tracker</h1>
+        <header className="header">
+            <h1>{title}</h1>
+            <Button text='Add' color='green' />
         </header>
     )
+}
+
+Header.defaultProps = {
+    title : 'Task Tracker',
+}
+
+Header.propTypes = {
+    title : PropTypes.string.isRequired,
 }
 
 export default Header
